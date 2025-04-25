7639ce6b3e4b35d189407f6edde6f9ecb8c4e84a
Sachithra Pinnaduwa <sachithrapinnaduwa@gmail.com>
2025-04-25 14:45:13 +0530

    fixed issues with login form style

--- public/partials/login-form.php
+++ public/partials/login-form.php
@@ -21,6 +21,10 @@ $redirect_to = isset($_REQUEST['redirect_to']) ? $_REQUEST['redirect_to'] : '';
 if (empty($redirect_to)) {
     $redirect_to = home_url();
 }
+
+// Allow theme styling to override plugin styles if enabled in settings
+$options = get_option('my_passwordless_auth_options', []);
+$theme_compat_class = isset($options['use_theme_styles']) && $options['use_theme_styles'] === 'yes' ? 'theme-compat' : '';
 ?>
 
 <div class="passwordless-login-container <?php echo esc_attr($theme_compat_class); ?>">
